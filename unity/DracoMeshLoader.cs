793d561b30aff488dd359a7cbbdb978e13f211ea
fanzhanggoogle <zhafang@google.com>
2017-10-27 09:26:43 -0700

    Added libs

--- unity/DracoMeshLoader.cs
+++ unity/DracoMeshLoader.cs
@@ -1,8 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-using System;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
@@ -11,7 +7,7 @@ using UnityEngine;
 public unsafe class DracoMeshLoader
 {
 	// Must stay the order to be consistent with C++ interface.
-	[ StructLayout( LayoutKind.Sequential )]
+	[ StructLayout (LayoutKind.Sequential)]
 	private struct DracoToUnityMesh
 	{
 		public int num_faces;
@@ -25,13 +21,77 @@ public unsafe class DracoMeshLoader
 		public float[] texcoord;
 	}
 
+	private struct DecodedMesh
+	{
+		public int[] faces;
+		public Vector3[] vertices;
+	}
+
 	[DllImport ("dracodec_unity")]
-	private static extern int DecodeMeshForUnity (byte[] buffer, int length, DracoToUnityMesh **tmp_mesh);
+	private static extern int DecodeMeshForUnity (byte[] buffer, int length, DracoToUnityMesh**tmp_mesh);
 
 	[DllImport ("dracodec_unity")]
 	private static extern int TestDecodingAndroid (byte[] buffer, int length);
 
-	private float ReadFloatFromIntPtr (IntPtr data, int offset) {
+	static private int max_num_vertices_per_mesh = 60000;
+
+	// Unity only support maximum 65534 vertices per mesh. So large meshes need to be splited.
+	private void SplitMesh (DecodedMesh mesh, ref List<DecodedMesh> splited_meshes)
+	{
+		List<int> faces_left = new List<int> ();
+		for (int i = 0; i < mesh.faces.Length; ++i) {
+			faces_left.Add (mesh.faces [i]);
+		}
+		int num_sub_meshes = 0;
+
+		while (faces_left.Count > 0) {
+			Debug.Log ("Faces left: " + faces_left.Count.ToString ());
+			num_sub_meshes++;
+			List<int> tmp_left_faces = new List<int> ();
+			List<int> faces_extracted = new List<int> ();
+			List<Vector3> vertices_extracted = new List<Vector3> ();
+
+			int unique_corner_id = 0;
+			Dictionary<int, int> index_to_new_index = new Dictionary<int ,int> ();
+			for (int face = 0; face < faces_left.Count / 3; ++face) {
+				bool pick_face = true;
+				// If all indices has appeared or there's still space for more vertices.
+				for (int corner = 0; corner < 3; ++corner) {
+					if (!index_to_new_index.ContainsKey (faces_left [face * 3 + corner])) {
+						if (unique_corner_id < max_num_vertices_per_mesh) {
+							index_to_new_index.Add (faces_left [face * 3 + corner], unique_corner_id);
+							vertices_extracted.Add (mesh.vertices [faces_left [face * 3 + corner]]);
+							unique_corner_id++;
+						} else {
+							// Can not take more new vertices.
+							pick_face = false;
+						}
+					}
+				}
+				// Add face to this sub mesh.
+				if (pick_face) {
+					for (int corner = 0; corner < 3; ++corner) {
+						faces_extracted.Add (index_to_new_index [faces_left [face * 3 + corner]]);
+					}
+				} else {
+					// Otherwise save face for the next sub-mesh.
+					for (int corner = 0; corner < 3; ++corner) {
+						tmp_left_faces.Add (faces_left [face * 3 + corner]);
+					}
+				}
+			}
+
+			DecodedMesh sub_mesh = new DecodedMesh ();
+			sub_mesh.faces = faces_extracted.ToArray ();
+			sub_mesh.vertices = vertices_extracted.ToArray ();
+			splited_meshes.Add (sub_mesh);
+
+			faces_left = tmp_left_faces;
+		}
+	}
+
+	private float ReadFloatFromIntPtr (IntPtr data, int offset)
+	{
 		byte[] byte_array = new byte[4];
 		for (int j = 0; j < 4; ++j) {
 			byte_array [j] = Marshal.ReadByte (data, offset + j);
@@ -39,8 +99,9 @@ public unsafe class DracoMeshLoader
 		return BitConverter.ToSingle (byte_array, 0);
 	}
 
-	public int LoadMeshFromAsset(string asset_name, ref Mesh mesh) {
-		TextAsset asset = Resources.Load(asset_name, typeof(TextAsset)) as TextAsset;
+	public int LoadMeshFromAsset (string asset_name, ref List<Mesh> meshes)
+	{
+		TextAsset asset = Resources.Load (asset_name, typeof(TextAsset)) as TextAsset;
 		if (asset == null) {
 			Debug.Log ("Didn't load file!");
 			return -1;
@@ -51,7 +112,7 @@ public unsafe class DracoMeshLoader
 			Debug.Log ("Didn't load bunny!");
 			return -1;
 		}
-		return DecodeMesh (bunny_data, ref mesh);
+		return DecodeMesh (bunny_data, ref meshes);
 	}
 	/*
 	public IEnumerator LoadMeshFromURL(string url, ref Mesh mesh) {
@@ -63,7 +124,8 @@ public unsafe class DracoMeshLoader
 	}
 	*/
 
-	public unsafe int DecodeMesh (byte[] data, ref Mesh mesh) {
+	public unsafe int DecodeMesh (byte[] data, ref List<Mesh> meshes)
+	{
 		DracoToUnityMesh* tmp_mesh;
 		if (DecodeMeshForUnity (data, data.Length, &tmp_mesh) <= 0) {
 			Debug.Log ("Failed: Decoding error.");
@@ -76,9 +138,9 @@ public unsafe class DracoMeshLoader
 		int num_faces = tmp_mesh->num_faces;
 		int[] new_triangles = new int[tmp_mesh->num_faces * 3];
 		for (int i = 0; i < tmp_mesh->num_faces; ++i) {
-			new_triangles[i * 3] = Marshal.ReadInt32 (tmp_mesh->indices, i * 3 * 4);
-			new_triangles[i * 3 + 1] = Marshal.ReadInt32 (tmp_mesh->indices, i * 3 * 4 + 4);
-			new_triangles[i * 3 + 2] = Marshal.ReadInt32 (tmp_mesh->indices, i * 3 * 4 + 8);
+			new_triangles [i * 3] = Marshal.ReadInt32 (tmp_mesh->indices, i * 3 * 4);
+			new_triangles [i * 3 + 1] = Marshal.ReadInt32 (tmp_mesh->indices, i * 3 * 4 + 4);
+			new_triangles [i * 3 + 2] = Marshal.ReadInt32 (tmp_mesh->indices, i * 3 * 4 + 8);
 		}
 
 		// For floating point numbers, there's no Marshal functions could directly read from the unmanaged data.
@@ -94,15 +156,37 @@ public unsafe class DracoMeshLoader
 			}
 		}
 
-		Marshal.FreeCoTaskMem(tmp_mesh->position);
-		Marshal.FreeCoTaskMem((IntPtr)tmp_mesh);
+		Marshal.FreeCoTaskMem (tmp_mesh->position);
+		Marshal.FreeCoTaskMem ((IntPtr)tmp_mesh);
 
-		mesh.vertices = new_vertices;     
-		mesh.triangles = new_triangles;
+		if (new_vertices.Length > 61000) {
+			DecodedMesh decoded_mesh = new DecodedMesh ();
+			decoded_mesh.vertices = new_vertices;
+			decoded_mesh.faces = new_triangles;
+			List<DecodedMesh> splited_meshes = new List<DecodedMesh> ();
 
-		mesh.RecalculateBounds();
-		mesh.RecalculateNormals();
+			SplitMesh (decoded_mesh, ref splited_meshes);
+			for (int i = 0; i < splited_meshes.Count; ++i) {
+				Mesh mesh = new Mesh ();
+				mesh.vertices = splited_meshes [i].vertices;
+				mesh.triangles = splited_meshes [i].faces;
 
+				mesh.RecalculateBounds ();
+				mesh.RecalculateNormals ();
+				meshes.Add (mesh);
+			}
+		} else {
+			// Mesh not that big.
+			Mesh mesh = new Mesh ();
+			mesh.vertices = new_vertices;
+			mesh.triangles = new_triangles;
+
+			// TODO: Duplicated code.
+			mesh.RecalculateBounds ();
+			mesh.RecalculateNormals ();
+			meshes.Add (mesh);
+		}
+			
 		return num_faces;
 	}
 }
